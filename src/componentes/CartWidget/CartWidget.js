122564d3673d3d149424ce294141d9ef63094ab9
Ariel Rueda <arielrueda13@gmail.com>
2022-03-30 11:04:15 -0300

    main 3.1

--- src/componentes/CartWidget/CartWidget.js
+++ src/componentes/CartWidget/CartWidget.js
@@ -1,9 +1,10 @@
 import  carrito from './carrito.png';
+import NavBar from '../NavBar.css';
 function CartWidget(params) {
     return(
 <div><img src={carrito} alt="carrito"/>
+<p className='carrito'>0</p>
      </div>
     );
-    
 }
 export default CartWidget
