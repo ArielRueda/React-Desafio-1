647ddbc318eb031db8c803f3677f685e21514f07
Ariel Rueda <arielrueda13@gmail.com>
2022-04-18 23:31:23 -0300

    main 8.1

--- src/componentes/ItemDetail/ItemDetail.js
+++ src/componentes/ItemDetail/ItemDetail.js
@@ -1,20 +1,23 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { getProductsById } from "../asyncMock";
 import Contador from '../ItemCount/ItemCount'
 import './ItemDetail.css'
 
 const ItemDetail = (products) => {
-    const handelOnAdd = (quantity ) => {
-        console.log(`se agregaron ${quantity} productos`);
+    const [quantity,setQuantity]=useState(0)
+    const handelOnAdd = (count) => {
+        console.log(`se agregaron al carrito `);
+setQuantity(count)
     }
     return (
         <div className="card1" >
-            <img className='imagen-card1'src={products.img} alt={products.name}  />
+            <img className='imagen-card1' src={products.img} alt={products.name} />
             <div className="card-body1">
                 <h3 className="name-card1" >{products.name}</h3>
                 <p className="card-text1">{products.description}</p>
                 <p className="card-price">${products.price}</p>
-                <Contador initial={0} stock={products.stock} onAdd={handelOnAdd} />
+               {quantity>0?<Link to='/cart' className="link-carrito">ir al carrito</Link>: <Contador initial={0} stock={products.stock} onAdd={handelOnAdd} />}
             </div>
         </div>
 
