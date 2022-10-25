48aa99e5dbdc3d1bcc87c81d6ddb23f67c1c47c4
Ariel Rueda <arielrueda13@gmail.com>
2022-10-25 02:12:26 -0300

    main 3.12

--- src/componentes/NavBar.js
+++ src/componentes/NavBar.js
@@ -1,7 +1,10 @@
 import logo from './imagenes/desktop-logo.png';
-import NavBarCss from './NavBar.css';
+import './NavBar.css';
 import CartWidget from './CartWidget/CartWidget';
 import { Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import { useContext } from 'react';
 import { useState, useEffect } from 'react';
 import CartContext from '../Context/CartContext.js';
@@ -24,35 +27,49 @@ function NavBar() {
         })
     }, [])
     return (
-
-        <nav id="barra" className="navbar navbar-expand-md navbar-light bg-light">
-            <div className="container-fluid">
-                <Link to='/' className='navbar-brand'> <img src={logo} alt="logo" /></Link>
+<Navbar id='barra' bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to='./'><img src={logo} alt="logo" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav  id="items"className="me-auto">
+            <Nav.Link as={Link} to='./' >Home</Nav.Link>
+            <Nav.Link as={Link} to='./productos'>Productos</Nav.Link>
+            <Nav.Link as={Link} to='./PreguntasFrecuentes'>FAQ</Nav.Link>
+            <Nav.Link  > {getQuantity()>0?<CartWidget />:null}</Nav.Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+        // <nav id="barra" className="navbar navbar-expand-md navbar-light bg-light">
+        //     <div className="container-fluid">
+        //         <Link to='/' className='navbar-brand'> <img src={logo} alt="logo" /></Link>
           
                    
             
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon">
-                    </span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div id="items" className="navbar-nav">
+        //         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        //             <span className="navbar-toggler-icon">
+        //             </span>
+        //         </button>
+        //         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        //             <div id="items" className="navbar-nav">
                      
-                         <Link to='/' className='nav-link'>Home</Link>
-                         <div>
-                    <Link to='/productos' className="navLink">Productos</Link>
-                    { categories.map(cat => <Link key={cat.id} to={`/category/${cat.id}`} className="navLink">{cat.description}</Link>) }
-                </div>
+        //                  <Link to='/' className='nav-link'>Home</Link>
+        //                  <div>
+        //             <Link to='/productos' className="navLink">Productos</Link>
+        //             { categories.map(cat => <Link key={cat.id} to={`/category/${cat.id}`} className="navLink">{cat.description}</Link>) }
+        //         </div>
                           
                         
-                      <Link to='/PreguntasFrecuentes' className='nav-link'>PreguntasFrecuentes</Link>
+        //               <Link to='/PreguntasFrecuentes' className='nav-link'>PreguntasFrecuentes</Link>
 
-                    </div>
-                </div>
-              {getQuantity()>0?<CartWidget />:null}
-            </div>
+        //             </div>
+        //         </div>
+        //       {getQuantity()>0?<CartWidget />:null}
+        //     </div>
 
-        </nav>
+        // </nav>
     );
 }
 export default NavBar
